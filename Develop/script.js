ebd3b3e713d84040f0362abf2ac709e8153bc6fe
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-22 06:28:03 -0500

    added day.js jquery

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,10 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // Ross: Added code to display the current date in the header of the page.
+  var time = dayjs().format('dddd, MMMM D YYYY');
+  $('#currentDay').text(time);
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,5 +23,4 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 });
