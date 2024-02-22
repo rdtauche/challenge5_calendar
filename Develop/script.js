57db8c57f599f01d87a17c0bbc99459305656381
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-22 07:06:56 -0500

    added refinement to save button

--- Develop/script.js
+++ Develop/script.js
@@ -5,12 +5,7 @@
   var dateToday = dayjs().format('dddd, MMMM D YYYY');
   $('#currentDay').text(dateToday);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Ross: Added an event listener for click events on the save button saved to local storage
   $(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
